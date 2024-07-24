1a4a6de41d4a0177569ef628b1a34116bf24eabf
Shawn Tan <shawn@wtf.sg>
2024-07-24 15:56:41 +0000

    Simple linear test.

--- test_linear_tp.py
+++ test_linear_tp.py
@@ -6,6 +6,8 @@ from transformers import set_seed
 
 from dolomite_engine.hf_models import AttentionHeadType, GPTDolomiteConfig, GPTDolomiteForCausalLM_TP
 from dolomite_engine.hf_models.modeling_utils_TP.linear import ColumnParallelLinear
+from dolomite_engine.hf_models.modeling_utils_TP.TP import tensor_parallel_split_safetensor_slice
+from dolomite_engine.hf_models.modeling_utils.linear import ParameterizedLinear
 from dolomite_engine.utils import (
     CUDA_RNGStatesTracker,
     ProcessGroupManager,
@@ -16,19 +18,45 @@ from dolomite_engine.utils import (
 
 
 set_seed(42)
-
-ProcessGroupManager(tensor_parallel_size=int(os.getenv("WORLD_SIZE")))
+tp_size = int(os.getenv("WORLD_SIZE"))
+ProcessGroupManager(tensor_parallel_size=tp_size)
 
 # this is needed when combining different kinds of parallelism for training
 # leave as is if unaware of what you are doing
-cuda_rng_tracker = CUDA_RNGStatesTracker()
-cuda_rng_tracker.add("tensor-parallel-seed", 42)
-set_cuda_rng_tracker(cuda_rng_tracker)
-
+# cuda_rng_tracker = CUDA_RNGStatesTracker()
+# cuda_rng_tracker.add("tensor-parallel-seed", 42)
+# set_cuda_rng_tracker(cuda_rng_tracker)
+tmp_path = "tmp/"
 torch_dtype = torch.bfloat16
+in_features = 1024
+out_features = 8192
+batch_size = 512
+
+model = ParameterizedLinear(
+    in_features=in_features,
+    out_features=out_features,
+    bias=False,
+    device=torch.cuda.current_device(),
+    dtype=torch_dtype,
+    std=0.02,
+)
+
+if torch.distributed.get_rank() == 0:
+    print("Initializing on device 0.")
+    weight = model.weight.data
+    input_tensor = torch.randn(batch_size, in_features, device=torch.cuda.current_device(), dtype=torch_dtype)
+else:
+    weight = torch.empty((out_features, in_features),
+                         dtype=torch_dtype, device=torch.cuda.current_device())
+    input_tensor = torch.empty((batch_size, in_features),
+                               device=torch.cuda.current_device(), dtype=torch_dtype)
+
+torch.distributed.broadcast(weight, 0)
+torch.distributed.broadcast(input_tensor, 0)
+
 model_tp = ColumnParallelLinear(
-    in_features=1024,
-    out_features=8192,
+    in_features=in_features,
+    out_features=out_features,
     bias=False,
     device=torch.device("cuda"),
     dtype=torch_dtype,
@@ -36,38 +64,24 @@ model_tp = ColumnParallelLinear(
     use_padding_free_transformer=False,
     sequence_parallel=False,
 )
+rank = torch.distributed.get_rank()
+
+model.load_state_dict({'weight': weight})
+
+weight = weight.view(tp_size, -1, in_features)
+model_tp.load_state_dict({'weight': weight[rank]})
+
 torch.distributed.barrier()
-
-
-# copy to device without copying storage
-model_tp = model_tp.to_empty(device=torch.cuda.current_device())
-
-# load weights into tensor parallel model using SafeTensorsWeightsManager class
-# this avoids loading multiple copies of the parameters in CPU memory
-safetensors_weight_manager = SafeTensorsWeightsManager("tmp/")
-model_tp.load_from_safetensors_weights_manager(safetensors_weight_manager)
 
 # set model to eval mode
 model_tp = model_tp.to(torch_dtype)
 model_tp.eval()
-
+model.eval()
 set_seed(42)
 
-input_tensor = torch.randn(512, 1024, device=torch.cuda.current_device())
 output_tp = model_tp(input_tensor)
-exit()
-if torch.distributed.get_rank() == 0:
-    output = model(input_ids=input_ids, labels=labels)
-    loss = output[0]
-    logits = output[1]
-
-    if args.use_padding_free_transformer:
-        logits_tp = logits_tp.reshape(batch_size, sequence_length, -1)
-
-    error = (logits - logits_tp).abs().max()
-    assert error < 5e-4, "logits don't match for normal and tensor parallel model"
-
-    error = (loss - loss_tp).abs().max()
-    assert error < 3e-6, "losses don't match for normal and tensor parallel model"
+output_ref = model(input_tensor)
+output_ref_chunk = output_ref.view(batch_size, tp_size, -1)[:, rank]
+print((output_tp - output_ref_chunk).abs().max())
 
 ProcessGroupManager.destroy_process_groups()
