b43e3717540d9dfc84e6651d0d1ede06689dd309
Shawn Tan <shawn@wtf.sg>
2024-07-25 10:23:25 -0400

    Figuring out scatter Tp

---
+++ dolomite_engine/hf_models/models/moe_dolomite/moe_TP/scatter.py
@@ -0,0 +1,280 @@
+import math
+from typing import Any, Mapping
+
+import torch
+import torch.nn as nn
+from torch.distributed._tensor.api import DTensor
+from torch.distributed._tensor.placement_types import Replicate, Shard
+
+from .....utils import ProcessGroupManager, SafeTensorsWeightsManager, is_scattermoe_available
+from ....enums import InitMethod
+from ....modeling_utils import ParameterizedLinear, get_activation_function, is_glu
+from ....modeling_utils_TP.TP import (
+    dtensor_to_tensor,
+    modify_state_dict_to_dtensor_dict,
+    tensor_parallel_split_safetensor_slice,
+    tensor_to_dtensor,
+)
+from ....utils import divide_if_divisible
+from ..config import MoEDolomiteConfig
+from ..moe.base import SparseMoE
+from ..moe.scatter import ScatterMoE, _ParameterizedScatteredExperts
+
+
+if is_scattermoe_available():
+    import scattermoe
+    from scattermoe.parallel_experts import parallel_linear as scattered_experts
+
+
+class ReplicatedParallelLinear(ParameterizedLinear):
+    def __init__(
+        self,
+        in_features: int,
+        out_features: int,
+        device: torch.device | None = None,
+        dtype: torch.dtype | None = None,
+        std: float | None = None,
+        use_padding_free_transformer: bool = False,
+        sequence_parallel: bool = False,
+    ) -> None:
+
+        super().__init__(
+            in_features=in_features,
+            out_features=out_features,
+            device=device,
+            dtype=dtype,
+            std=std,
+        )
+
+        self.weight = nn.Parameter(
+            DTensor.from_local(
+                self.weight, device_mesh=ProcessGroupManager.get_tensor_parallel_mesh(), placements=[Replicate()]
+            )
+        )
+
+        if sequence_parallel:
+            if use_padding_free_transformer:
+                self.input_placement = Shard(0)
+            else:
+                self.input_placement = Shard(1)
+        else:
+            self.input_placement = Replicate()
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        input = tensor_to_dtensor(input, current_placement=self.input_placement)
+        input = super().forward(input)
+        input = dtensor_to_tensor(input, desired_placement=Replicate())
+        return input
+
+    def load_from_safetensors_weights_manager(
+        self, safetensors_weight_manager: SafeTensorsWeightsManager, prefix: str = ""
+    ) -> None:
+        weight = safetensors_weight_manager.get_slice(prefix + "weight")
+        weight = tensor_parallel_split_safetensor_slice(weight, dim=0)
+        state_dict = {"weight": weight}
+
+        if self.bias is not None:
+            bias = safetensors_weight_manager.get_slice(prefix + "bias")
+            bias = tensor_parallel_split_safetensor_slice(bias, dim=0)
+            state_dict["bias"] = bias
+
+        self.load_state_dict(state_dict)
+
+    def extra_repr(self) -> str:
+        return "in_features={}, out_features_per_device={}, bias={}".format(
+            self.in_features, self.out_features_per_device, self.bias is not None
+        )
+
+    def load_state_dict(self, state_dict: Mapping[str, Any], strict: bool = True, assign: bool = False) -> None:
+        state_dict = modify_state_dict_to_dtensor_dict(self, state_dict)
+        return super().load_state_dict(state_dict, strict, assign)
+
+
+class ScatterMoETP(SparseMoE):
+    def __init__(
+        self, config: MoEDolomiteConfig, use_padding_free_transformer: bool, layer_idx: int | None = None
+    ) -> None:
+        nn.Module.__init__(self)
+
+        self.hidden_size = config.hidden_size
+
+        self.num_experts = config.num_experts
+        self.top_k = config.num_experts_per_tok
+        self.normalize_expert_weights = config.normalize_expert_weights
+        self.use_padding_free_transformer = use_padding_free_transformer
+        self.layer_idx = layer_idx
+
+        self.gate = ParameterizedLinear(self.hidden_size, self.num_experts, bias=False)
+
+        intermediate_size = config.n_inner
+        activation_function = config.activation_function
+        residual_dropout = config.resid_pdrop
+
+        assert not config.add_bias, "ScatterMoE does not support add_bias"
+
+        initializer_range = config.initializer_range
+        m_width = config.m_width
+        n_layer = config.n_layer
+        init_method = InitMethod(config.init_method)
+
+        std = initializer_range
+        if init_method == InitMethod.mup:
+            std /= math.sqrt(m_width)
+        self.c_fc = _ParameterizedScatteredExperts(
+            self.num_experts,
+            self.hidden_size,
+            2 * intermediate_size if is_glu(activation_function) else intermediate_size,
+            std=std,
+        )
+
+        self.act = get_activation_function(activation_function)
+
+        std = initializer_range / math.sqrt(2 * n_layer)
+        if init_method == InitMethod.mup:
+            std /= math.sqrt(m_width)
+        self.c_proj = _ParameterizedScatteredExperts(self.num_experts, intermediate_size, self.hidden_size, std=std)
+
+        self.dropout = nn.Identity() if residual_dropout == 0 else nn.Dropout(residual_dropout)
+
+    def _compute_expert_outputs(
+        self, hidden_states: torch.Tensor, routing_weights: torch.Tensor, selected_experts: torch.Tensor
+    ) -> torch.Tensor:
+        with torch.no_grad():
+            sorted_expert_idxs, sorted_scattered_idxs = scattermoe.kernels.ops.flatten_and_sort(selected_experts)
+            padded_block_idxs, expert_offsets = scattermoe.kernels.ops.padded_block_indices(
+                sorted_expert_idxs, self.num_experts
+            )
+
+        hidden_states = self.c_fc(
+            hidden_states,
+            self.top_k,
+            sorted_expert_idxs,
+            sorted_scattered_idxs,
+            padded_block_idxs,
+            expert_offsets,
+            grouped_out=True,
+        )
+        hidden_states = self.act(hidden_states)
+        hidden_states = self.c_proj(
+            hidden_states,
+            1,
+            sorted_expert_idxs,
+            sorted_scattered_idxs,
+            padded_block_idxs,
+            expert_offsets,
+            grouped_in=True,
+            gates=routing_weights,
+        )
+        hidden_states = self.dropout(hidden_states)
+        return hidden_states
+
+
+class _ColumnParallelScatteredExperts(_ParameterizedScatteredExperts):
+    def __init__(self, num_experts: int, input_size: int, output_size: int, std: float | None = None) -> None:
+        tp_world_size = ProcessGroupManager.get_tensor_parallel_world_size()
+        self.out_features_per_device = divide_if_divisible(
+            output_size,
+            tp_world_size,
+            f"`output_size` ({output_size}) must be divisible by `tensor_parallel_world_size` ({tp_world_size})",
+        )
+        super().__init__(
+            num_experts=num_experts,
+            output_size=self.out_features_per_device,
+            input_size=input_size,
+            std=std,
+        )
+        self.weight = nn.Parameter(
+            DTensor.from_local(
+                self.weight, device_mesh=ProcessGroupManager.get_tensor_parallel_mesh(), placements=[Shard(1)]
+            )
+        )
+        self.input_placement = Replicate()
+
+    def forward(
+        self,
+        inputs,
+        k,
+        sorted_expert_idxs,
+        sorted_scattered_idxs,
+        padded_block_idxs,
+        expert_offsets,
+        gates=None,
+        grouped_in=False,
+        grouped_out=False,
+    ):
+        inputs = tensor_to_dtensor(inputs, current_placement=self.input_placement)
+        sorted_expert_idxs = tensor_to_dtensor(sorted_expert_idxs, current_placement=self.input_placement)
+        sorted_scattered_idxs = tensor_to_dtensor(sorted_scattered_idxs, current_placement=self.input_placement)
+        padded_block_idxs = tensor_to_dtensor(padded_block_idxs, current_placement=self.input_placement)
+        expert_offsets = tensor_to_dtensor(expert_offsets, current_placement=self.input_placement)
+        gates = tensor_to_dtensor(gates, current_placement=self.input_placement)
+        results = scattered_experts(
+            inputs,
+            self.weight.permute(0, 2, 1),
+            k,
+            sorted_expert_idxs,
+            sorted_scattered_idxs,
+            padded_block_idxs,
+            expert_offsets,
+            gates,
+            grouped_in,
+            grouped_out,
+        )
+        results = dtensor_to_tensor(results, desired_placement=Shard(-1))
+        return results
+
+
+class _RowParallelScatteredExperts(_ParameterizedScatteredExperts):
+    def __init__(self, num_experts: int, input_size: int, output_size: int, std: float | None = None) -> None:
+        tp_world_size = ProcessGroupManager.get_tensor_parallel_world_size()
+        self.in_features_per_device = divide_if_divisible(
+            input_size,
+            tp_world_size,
+            f"`output_size` ({input_size}) must be divisible by `tensor_parallel_world_size` ({tp_world_size})",
+        )
+        super().__init__(
+            num_experts=num_experts,
+            output_size=self.in_features_per_device,
+            input_size=input_size,
+            std=std,
+        )
+        self.weight = nn.Parameter(
+            DTensor.from_local(
+                self.weight, device_mesh=ProcessGroupManager.get_tensor_parallel_mesh(), placements=[Shard(2)]
+            )
+        )
+        self.input_placement = Shard(-1)
+        self.output_placement = Replicate()
+
+    def forward(
+        self,
+        inputs,
+        k,
+        sorted_expert_idxs,
+        sorted_scattered_idxs,
+        padded_block_idxs,
+        expert_offsets,
+        gates=None,
+        grouped_in=False,
+        grouped_out=False,
+    ):
+        inputs = tensor_to_dtensor(inputs, current_placement=self.input_placement)
+        sorted_expert_idxs = tensor_to_dtensor(sorted_expert_idxs, current_placement=self.input_placement)
+        sorted_scattered_idxs = tensor_to_dtensor(sorted_scattered_idxs, current_placement=self.input_placement)
+        padded_block_idxs = tensor_to_dtensor(padded_block_idxs, current_placement=self.input_placement)
+        expert_offsets = tensor_to_dtensor(expert_offsets, current_placement=self.input_placement)
+        gates = tensor_to_dtensor(gates, current_placement=self.input_placement)
+        results = scattered_experts(
+            inputs,
+            self.weight.permute(0, 2, 1),
+            k,
+            sorted_expert_idxs,
+            sorted_scattered_idxs,
+            padded_block_idxs,
+            expert_offsets,
+            gates,
+            grouped_in,
+            grouped_out,
+        )
+        results = dtensor_to_tensor(results, desired_placement=self.output_placement)
+        return results
