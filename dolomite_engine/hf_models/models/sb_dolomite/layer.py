21b2d9d32d6a8f350b13a58848dd00c2f0dde850
Shawn Tan <shawn@wtf.sg>
2024-08-07 12:11:54 +0800

    Include attention module in to package, fix attention implementation to padding-free stickbreaking.

--- dolomite_engine/hf_models/models/sb_dolomite/layer.py
+++ dolomite_engine/hf_models/models/sb_dolomite/layer.py
@@ -7,6 +7,114 @@ from ...modeling_utils import get_attention_module, get_normalization_function
 from .config import GPTDolomiteConfig
 from .mlp import MLP
 
+from ...modeling_utils.attention import  Attention
+from flash_attn.flash_attn_interface import flash_attn_varlen_func
+
+
+class PaddingFreeSBAttention(Attention):
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        past_key_values: DynamicCache | None = None,
+        attention_mask: torch.Tensor | None = None,
+        rope_cos_sin: torch.Tensor | None = None,
+        cu_seqlens: torch.Tensor | None = None,
+        max_seqlen: torch.Tensor | None = None,
+    ) -> torch.Tensor:
+        assert past_key_values is None
+
+        # ==========================================================================================
+        # hidden_states -> (total_q, num_heads * head_dim)
+        # ==========================================================================================
+
+        query, key, value = self._prepare_qkv_for_forward(hidden_states)
+
+        # ==========================================================================================
+        # query -> (total_q, num_heads, head_dim)
+        # key -> (total_q, num_key_value_heads, head_dim)
+        # value -> (total_q, num_key_value_heads, head_dim)
+        # ==========================================================================================
+
+        # if self.position_embedding_type == PositionEmbeddingType.rope:
+        #     query = apply_rotary_pos_emb(query, rope_cos_sin)
+        #     key = apply_rotary_pos_emb(key, rope_cos_sin)
+
+        # ==========================================================================================
+        # query -> (total_q, num_heads, head_dim)
+        # key -> (total_q, num_key_value_heads, head_dim)
+        # value -> (total_q, num_key_value_heads, head_dim)
+        # ==========================================================================================
+
+        softmax_scale = self._get_softmax_scale()
+        dropout_p = self.attn_pdrop if self.training else 0
+
+        attn_output = flash_attn_varlen_func(
+            query,
+            key,
+            value,
+            cu_seqlens_q=cu_seqlens,
+            cu_seqlens_k=cu_seqlens,
+            max_seqlen_q=max_seqlen,
+            max_seqlen_k=max_seqlen,
+            dropout_p=dropout_p,
+            softmax_scale=softmax_scale,
+            causal=self.causal,
+        )
+
+        # ==========================================================================================
+        # attn_output -> (total_q, num_heads, head_dim)
+        # ==========================================================================================
+
+        attn_output = attn_output.view(-1, self.hidden_size)
+
+        # ==========================================================================================
+        # attn_output -> (total_q, num_heads * head_dim)
+        # ==========================================================================================
+
+        attn_output = self.c_proj(attn_output)
+        attn_output = self.resid_dropout(attn_output)
+
+        return attn_output
+
+    def _prepare_qkv_for_forward_mha(
+        self, hidden_states: torch.Tensor
+    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        total_q = hidden_states.shape[0]
+
+        hidden_states = hidden_states.view(total_q, self.num_key_value_heads, -1)
+        query, key, value = hidden_states.chunk(3, dim=-1)
+
+        return query, key, value
+
+    def _prepare_qkv_for_forward_gqa(
+        self, hidden_states: torch.Tensor
+    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        total_q = hidden_states.shape[0]
+
+        hidden_states = hidden_states.view(total_q, self.num_key_value_heads, -1)
+
+        query, key, value = hidden_states.split(
+            ((self.num_heads // self.num_key_value_heads) * self.head_dim, self.head_dim, self.head_dim), dim=-1
+        )
+
+        # this needs to be a reshape instead of view sadly
+        query = query.reshape(total_q, -1, self.head_dim)
+
+        return query, key, value
+
+    def _prepare_qkv_for_forward_mqa(
+        self, hidden_states: torch.Tensor
+    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        total_q = hidden_states.shape[0]
+
+        query, key, value = hidden_states.split((self.hidden_size, self.head_dim, self.head_dim), dim=-1)
+
+        query = query.view(total_q, self.num_heads, -1)
+        key = key.unsqueeze(1)
+        value = value.unsqueeze(1)
+
+        return query, key, value
+
 
 class GPTDolomiteBlock(nn.Module):
     """
@@ -35,9 +143,14 @@ class GPTDolomiteBlock(nn.Module):
             eps=config.layer_norm_epsilon,
             normalization_implementation=normalization_implementation,
         )
-        self.attn = get_attention_module(
-            config, True, attention_implementation, use_padding_free_transformer, layer_idx
-        )
+
+        assert use_padding_free_transformer
+        print("Directly load PaddingFreeSB")
+
+        # self.attn = get_attention_module(
+        #     config, True, attention_implementation, use_padding_free_transformer, layer_idx
+        # )
+        self.attn = PaddingFreeSBAttention(config, causal=True, layer_idx=layer_idx)
         self.ln_2 = get_normalization_function(
             config.normalization_function,
             hidden_size,
