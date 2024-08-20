5951a821270637f94daed16c5f26de75d246a34b
Shawn Tan <shawn@wtf.sg>
2024-08-20 13:46:52 -0400

    Testing script.

--- test_scattermoe_tp.py
+++ test_scattermoe_tp.py
@@ -28,30 +28,20 @@ torch_dtype = torch.float32
 # k = 2
 # in_features = 1024
 # out_features = 1024
-# batch_size = 512
-local_moe = ScatterMoE(config, use_padding_free_transformer=False, layer_idx=0)
-print(local_moe)
+batch_size = 512
 rank = torch.distributed.get_rank()
-if rank == 0:
-    print("Initializing on device ", rank)
-    weight = model.weight.data
-    input_tensor = torch.randn(k * batch_size, in_features, device=torch.cuda.current_device(), dtype=torch_dtype)
-    logits = torch.randn(batch_size, num_experts, device=torch.cuda.current_device(), dtype=torch_dtype)
-    expert_p, expert_idxs = torch.topk(torch.softmax(logits, dim=-1), k=k)
-    expert_idxs = expert_idxs.to(dtype=torch.int32).to(device=torch.cuda.current_device())
-else:
-    weight = torch.empty(
-        (num_experts, out_features, in_features), dtype=torch_dtype, device=torch.cuda.current_device()
-    )
-    input_tensor = torch.empty((k * batch_size, in_features), device=torch.cuda.current_device(), dtype=torch_dtype)
-    expert_idxs = torch.empty((batch_size, k), device=torch.cuda.current_device(), dtype=torch.int32)
-    expert_p = torch.empty((batch_size, k), device=torch.cuda.current_device(), dtype=torch_dtype)
+local_moe = ScatterMoE(config, use_padding_free_transformer=False, layer_idx=0)
 
+input_tensor = torch.randn(batch_size, config.n_embd, device=torch.cuda.current_device(), dtype=torch_dtype)
+gate_weight = local_moe.gate.weight
+c_fc_weight = local_moe.c_fc.weight
+c_proj_weight = local_moe.c_proj.weight
 
-torch.distributed.broadcast(weight, 0)
 torch.distributed.broadcast(input_tensor, 0)
-torch.distributed.broadcast(expert_idxs, 0)
-torch.distributed.broadcast(expert_p, 0)
+torch.distributed.broadcast(gate_weight, 0)
+torch.distributed.broadcast(c_fc_weight, 0)
+torch.distributed.broadcast(c_proj_weight, 0)
+
 
 model_tp = _RowParallelScatteredExperts(
     num_experts=num_experts,
