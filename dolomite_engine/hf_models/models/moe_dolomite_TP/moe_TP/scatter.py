eb1f4e2cc7aac142977b1aca446961c7224aa879
Shawn Tan <shawn@wtf.sg>
2024-08-22 15:30:41 -0400

    Replace replicated linear.

--- dolomite_engine/hf_models/models/moe_dolomite_TP/moe_TP/scatter.py
+++ dolomite_engine/hf_models/models/moe_dolomite_TP/moe_TP/scatter.py
@@ -17,6 +17,7 @@ from ....modeling_utils_TP import (
     modify_state_dict_to_dtensor_dict,
     tensor_to_dtensor,
 )
+from ....modeling_utils_TP.linear import ReplicatedLinear
 from ....utils import divide_if_divisible
 from ...moe_dolomite.config import MoEDolomiteConfig
 from ...moe_dolomite.moe.base import SparseMoE
@@ -26,44 +27,6 @@ from ...moe_dolomite.moe.scatter import ParameterizedScatteredExperts
 if is_scattermoe_available():
     import scattermoe
     from scattermoe.parallel_experts import parallel_linear as scattered_experts
-
-
-class ReplicatedParallelLinear(ParameterizedLinear):
-    def __init__(
-        self,
-        in_features: int,
-        out_features: int,
-        device: torch.device | None = None,
-        dtype: torch.dtype | None = None,
-        std: float | None = None,
-        use_padding_free_transformer: bool = False,
-        sequence_parallel: bool = False,
-    ) -> None:
-        super().__init__(
-            in_features=in_features, out_features=out_features, device=device, dtype=dtype, std=std, bias=False
-        )
-
-        self.weight = nn.Parameter(
-            DTensor.from_local(
-                self.weight, device_mesh=ProcessGroupManager.get_tensor_parallel_mesh(), placements=[Replicate()]
-            )
-        )
-        self.input_placement = Replicate()
-
-    def forward(self, input: torch.Tensor) -> torch.Tensor:
-        input = tensor_to_dtensor(input, current_placement=self.input_placement)
-        input = super().forward(input)
-        input = dtensor_to_tensor(input, desired_placement=Replicate())
-        return input
-
-    def extra_repr(self) -> str:
-        return "in_features={}, out_features_per_device={}, bias={}".format(
-            self.in_features, self.out_features, self.bias is not None
-        )
-
-    def load_state_dict(self, state_dict: Mapping[str, Any], strict: bool = True, assign: bool = False) -> None:
-        state_dict = modify_state_dict_to_dtensor_dict(self, state_dict)
-        return super().load_state_dict(state_dict, strict, assign)
 
 
 class ColumnParallelScatteredExperts(ParameterizedScatteredExperts):
@@ -253,12 +216,15 @@ class ScatterMoETP(SparseMoE):
         init_method = InitMethod(config.init_method)
         residual_dropout = config.resid_pdrop
 
-        self.gate = ReplicatedParallelLinear(
+        self.gate = ReplicatedLinear(
             in_features=self.hidden_size,
-            out_features=config.num_experts,
-            std=config.initializer_range,
+            out_features=self.intermediate_size,
+            bias=False,
             device=device,
             dtype=dtype,
+            std=config.initializer_range,
+            use_padding_free_transformer=use_padding_free_transformer,
+            sequence_parallel=False,
         )
 
         std = initializer_range
