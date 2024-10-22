74187eb0acdbcec8daaf5e0eda261b69f9ad9bf9
Shawn Tan <shawntan@Shawns-MacBook-Pro.local>
2024-10-22 17:44:44 -0400

    Refactored get_moe

---
+++ dolomite_engine/hf_models/models/moe_dolomite/moe/utils.py
@@ -0,0 +1,17 @@
+from ..config import MoEDolomiteConfig
+from .base import Experts
+from .scatter import ScatterExperts
+
+_MOE_MODULES = {"eager": Experts, "scattermoe": ScatterExperts}
+
+
+def get_moe(
+    config: MoEDolomiteConfig,
+    moe_implementation: str,
+    use_padding_free_transformer: bool,
+    layer_idx: int,
+) -> Experts:
+    if moe_implementation in _MOE_MODULES:
+        return _MOE_MODULES[moe_implementation](config, use_padding_free_transformer, layer_idx=layer_idx)
+
+    raise ValueError(f"unexpected `moe_implementation` {moe_implementation}")
