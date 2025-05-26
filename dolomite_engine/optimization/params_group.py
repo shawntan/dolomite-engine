80a0e5f2fe60482d61bc4cf174dd8c1b2f0dad6b
root <root@slurm-gb200-221-167.slurm-compute.tenant-slurm.svc.cluster.local>
2025-05-26 18:11:27 +0000

    Remove import

--- dolomite_engine/optimization/params_group.py
+++ dolomite_engine/optimization/params_group.py
@@ -12,7 +12,6 @@ from ..hf_models import (
     is_parameter_with_mup_learning_rate,
     is_parameter_with_no_weight_decay,
 )
-from ..hf_models.modeling_utils import RNN, Attention, Mamba2, MoE
 from ..model_wrapper import ModelWrapper
 from ..utils import BaseArgs, log_rank_0
 
