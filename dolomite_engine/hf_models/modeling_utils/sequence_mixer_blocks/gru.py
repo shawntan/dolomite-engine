Wrong GroupNorm normalisation

dolomite_engine/hf_models/modeling_utils/sequence_mixer_blocks/gru.py
@@ -42,8 +42,7 @@ class GroupedLinear(nn.Module):
         self.groups = groups
         self.in_dim = divide_if_divisible(in_channels, groups, "in_channels must be divisible by groups")
         self.out_dim = divide_if_divisible(out_channels, groups, "out_channels must be divisible by groups")
-
-        self.weight = nn.Parameter(torch.empty(self.groups, self.in_channels, self.out_channels))
+        self.weight = nn.Parameter(torch.empty(self.groups, self.in_dim, self.out_dim))
         self.reset_parameters()
 
         # mark_parameter_as_no_weight_decay(self.bias)
@@ -58,10 +57,18 @@ class GroupedLinear(nn.Module):
                 self.bias.zero_()
 
     def forward(self, x):
-        x_size = x.size()
-        x = x.view(-1, self.groups, self.in_dim)
-        y = torch.matmul(x[:, :, None], self.weight)
-        y = y.view(*(x_size[:-1]), self.out_channels)
+        x_size = x.size() 
+        # X: ..., groups * in_dim
+        x = x.view(-1, self.groups, self.in_dim) 
+        # X: ..., groups, in_dim
+        x = x.transpose(1, 0)
+        # X: groups,  *, in_dim
+        # weight: groups, in_dim, out_dim
+        y = torch.bmm(x, self.weight)
+        # y: groups, *, out_dim
+        y = y.transpose(1, 0)
+        # y: *, groups, out_dim
+        y = y.reshape(*(x_size[:-1]), self.out_channels)
         return y
 
 
@@ -101,7 +108,7 @@ class GRU(nn.Module):
 
         self.input_projection = ParameterizedLinear(self.input_size, self.input_size, bias=add_bias, std=std)
 
-        self.head_activation = get_activation_function("tanh")
+        self.head_activation = get_activation_function(head_activation)
         # self.head_projection = ParameterizedConv1d(
         #     in_channels=self.input_size,
         #     out_channels=3 * self.state_size,
@@ -122,13 +129,20 @@ class GRU(nn.Module):
         std = initializer_range / math.sqrt(2 * num_layers)
         if init_method == "mup":
             std /= math.sqrt(m_width)
-        self.output_head_projection = ParameterizedConv1d(
+        # self.output_head_projection = ParameterizedConv1d(
+        #     in_channels=self.state_size,
+        #     out_channels=self.input_size,
+        #     kernel_size=1,
+        #     groups=self.num_heads,
+        #     bias=add_bias,
+        #     std=std,
+        # )
+
+        self.output_head_projection = GroupedLinear(
             in_channels=self.state_size,
             out_channels=self.input_size,
-            kernel_size=1,
             groups=self.num_heads,
-            bias=add_bias,
-            std=std,
+            std=std
         )
         self.ln_output_head = nn.GroupNorm(num_groups=self.num_heads, num_channels=self.input_size)
 
@@ -196,6 +210,9 @@ class GRU(nn.Module):
             cu_seqlens=cu_seqlens,
             max_seqlen=max_seqlen,
         )
+        input = self.output_head_projection(input.flatten(-2, -1))
+        input_size = input.size()
+        input = self.ln_output_head(input.flatten(0, 1)).view(input_size)
 
         if not self.use_padding_free_transformer and attention_mask is not None:
             input = unpack_sequence(
@@ -206,10 +223,6 @@ class GRU(nn.Module):
             input_state = input[:, -1].view(input.size(0), -1)
             cache_params.update(state=input_state, num_tokens_added=input.size(1), layer_idx=self.layer_idx)
 
-        input = input.view(*input.size()[:-2], -1)
-
-        input = self.output_head_projection(input.transpose(1, 2))
-        input = self.ln_output_head(input).transpose(1, 2)
 
         input = self.output_projection(input)
 