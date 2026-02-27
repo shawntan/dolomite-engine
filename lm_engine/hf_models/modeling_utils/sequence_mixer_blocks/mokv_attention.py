68ad72012d8021ab452908bb2769d7f7dc5e53db
Shawn Tan <shawntan@ibm.com>
2026-02-27 12:46:04 +0000

    Combined with ParallelExpert.

--- lm_engine/hf_models/modeling_utils/sequence_mixer_blocks/mokv_attention.py
+++ lm_engine/hf_models/modeling_utils/sequence_mixer_blocks/mokv_attention.py
@@ -22,6 +22,14 @@ from ..position_embedding import apply_rotary_pos_emb
 from .utils import flash_attention
 
 from .mokv.triton import attention as mokv_attention
+from ..mlp_blocks.mlp import _get_std_for_linear
+from ....utils import ProcessGroupManager, is_xma_available
+
+from ..mlp_blocks.moe import compute_bincount, ParameterizedExperts
+from ...loss import add_aux_loss
+
+if is_xma_available():
+    from xma import continuous_count
 
 class MoKVAttention(nn.Module):
     def __init__(
@@ -49,12 +57,13 @@ class MoKVAttention(nn.Module):
         self.causal = causal
         self.hidden_size = hidden_size
         self.num_heads = num_attention_heads
-        # self.num_key_value_heads = num_key_value_heads
-        self.num_key_value_heads = num_attention_heads
+        self.num_key_value_heads = num_key_value_heads
+        # self.num_key_value_heads = num_attention_heads
         self.add_bias = add_bias
         self.qkv_bias = qkv_bias
         self.use_padding_free_transformer = use_padding_free_transformer
         self.sliding_window = sliding_window
+        # self.top_k = num_key_value_heads
 
         self.head_dim = divide_if_divisible(
             self.hidden_size,
@@ -72,15 +81,32 @@ class MoKVAttention(nn.Module):
             f"`num_heads` ({self.num_heads}) should be a multiple of `num_key_value_heads` ({self.num_key_value_heads})",
         )
 
+        std = _get_std_for_linear(initializer_range, init_method, m_width)
+        self.gate = ParameterizedLinear(
+            in_features=self.hidden_size,
+            out_features=self.num_heads,
+            bias=False,
+            std=std,
+        )
+
+
         std = initializer_range
         if init_method == "mup":
             std /= math.sqrt(m_width)
-        self.c_attn = ParameterizedLinear(
+        self.c_attn_q = ParameterizedLinear(
             self.hidden_size,
-            self.hidden_size + 2 * self.num_key_value_heads * self.head_dim,
+            self.hidden_size,
             bias=self.qkv_bias,
             std=std,
         )
+        self.c_attn_kv = ParameterizedExperts(
+            num_experts=self.num_heads,
+            in_features=self.hidden_size,
+            out_features=2 * self.head_dim,
+            add_bias=self.qkv_bias,
+            std=std,
+        )
+
 
         std = initializer_range / math.sqrt(2 * num_layers)
         if init_method == "mup":
@@ -91,9 +117,62 @@ class MoKVAttention(nn.Module):
 
         self.softmax_dropout = Dropout(softmax_dropout)
         self.dropout = Dropout(dropout)
+        self.is_hopper_or_newer_gpu = torch.cuda.is_available() and torch.cuda.get_device_capability(
+            torch.cuda.current_device()
+        ) >= (9, 0)
 
-        mark_parameter_as_mup_learning_rate(self.c_attn.weight)
+
+
+        mark_parameter_as_mup_learning_rate(self.c_attn_q.weight)
+        mark_parameter_as_mup_learning_rate(self.c_attn_kv.weight)
         mark_parameter_as_mup_learning_rate(self.c_proj.weight)
+
+    def _get_topk(self, x: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
+        x, indices = x.topk(self.num_key_value_heads, dim=-1)
+        return x, indices
+    def _compute_routing_weights(self, hidden_states: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        # hidden_states -> (total_q, hidden_size)
+        router_logits = self.gate(hidden_states)
+        # router_logits -> (total_q, num_experts)
+
+        # if self.normalized_topk:
+        router_weights, selected_experts = self._get_topk(router_logits)
+        router_weights = F.softmax(router_weights.float(), dim=-1)
+        router_weights = router_weights.type_as(hidden_states)
+        # else:
+        #     router_weights = F.softmax(router_logits.float(), dim=-1)
+        #     router_weights = router_weights.type_as(hidden_states)
+        #     router_weights, selected_experts = self._get_topk(router_weights)
+
+        return router_logits, router_weights, selected_experts
+    def _compute_switch_loss(
+        self, logits: torch.Tensor, probs: torch.Tensor, expert_frequency: torch.Tensor
+    ) -> torch.Tensor:
+        logits = logits.view(-1, logits.size(-1))
+        probs = probs.view(-1, probs.size(-1))
+
+        num_experts = logits.size(1)
+        acc_probs = probs.sum(0)
+
+        expert_frequency = expert_frequency.float()
+
+        if ProcessGroupManager.is_initialized() and ProcessGroupManager.get_data_parallel_world_size() > 1:
+            expert_frequency = all_reduce(
+                expert_frequency, reduceOp="sum", group=ProcessGroupManager.get_data_parallel_group()
+            )
+
+        switch_loss = (
+            num_experts * (F.normalize(acc_probs, p=1, dim=0) * F.normalize(expert_frequency, p=1, dim=0)).sum()
+        )
+        z_loss = (torch.logsumexp(logits, dim=-1) ** 2).mean()
+
+        loss = switch_loss + 0.1 * z_loss
+
+        return loss.type_as(logits)
+
+
+
+
 
     def forward(
         self,
@@ -104,8 +183,8 @@ class MoKVAttention(nn.Module):
         cu_seqlens: torch.Tensor | None = None,
         max_seqlen: int | None = None,
     ) -> torch.Tensor:
-        use_flash_attention_2 = is_kernel_allowed(Kernel.flash_attention_2)
-        use_flash_attention_3 = is_kernel_allowed(Kernel.flash_attention_3)
+        # use_flash_attention_2 = is_kernel_allowed(Kernel.flash_attention_2)
+        # use_flash_attention_3 = is_kernel_allowed(Kernel.flash_attention_3)
         accelerator = Accelerator.get_accelerator()
         # print("HEllo using new mokv_attention")
         # if self.use_padding_free_transformer:
@@ -122,18 +201,42 @@ class MoKVAttention(nn.Module):
         input_shape = (batch_size, query_length, self.num_key_value_heads, -1)
         output_shape = (batch_size, query_length, -1, self.head_dim)
 
-        hidden_states = self.c_attn(hidden_states)
-        hidden_states = hidden_states.view(*input_shape)
+        router_logits, router_weights, selected_experts = self._compute_routing_weights(hidden_states)
+        with torch.no_grad():
+            sorted_expert_idxs, sorted_scattered_idxs = selected_experts.flatten().sort()
+            expert_frequency = compute_bincount(
+                x=sorted_expert_idxs,
+                size=self.num_heads,
+                use_continuous_count=self.is_hopper_or_newer_gpu and is_kernel_allowed(Kernel.continuous_count),
+            )
+            expert_offsets = expert_frequency.cumsum(-1)
 
-        query, key, value = (
-            contiguous_split if Accelerator.get_accelerator() == Accelerator.trainium else torch.split
-        )(
-            hidden_states,
-            ((self.num_heads // self.num_key_value_heads) * self.head_dim, self.head_dim, self.head_dim),
-            dim=-1,
+        q_heads = self.c_attn_q(hidden_states)
+        
+        kv_heads = self.c_attn_kv(
+            input=hidden_states.flatten(0, 1),
+            num_experts_per_token=self.num_key_value_heads,
+            sorted_expert_idxs=sorted_expert_idxs,
+            sorted_scattered_idxs=sorted_scattered_idxs,
+            expert_offsets=expert_offsets,
+            grouped_out=False,
         )
 
-        query = query.reshape(*output_shape)
+        query = q_heads.view(*output_shape)
+        # print("Before reshape:", kv_heads.size())
+        kv_heads = kv_heads.view(*input_shape)
+        # print("After reshape: ", kv_heads.size())
+        key, value = kv_heads.chunk(2, dim=-1)
+        value = router_weights.unsqueeze(-1) * value
+        # query, key, value = (
+        #     contiguous_split if Accelerator.get_accelerator() == Accelerator.trainium else torch.split
+        # )(
+        #     hidden_states,
+        #     ((self.num_heads // self.num_key_value_heads) * self.head_dim, self.head_dim, self.head_dim),
+        #     dim=-1,
+        # )
+
+        # print(query.size())
 
         if not self.use_padding_free_transformer:
             query = query.transpose(1, 2)
@@ -176,12 +279,13 @@ class MoKVAttention(nn.Module):
         #     sliding_window=self.sliding_window,
         # )
         assert self.causal
-        kv_idxs = torch.arange(self.num_heads, dtype=torch.int32, device=query.device)[None, None, :].repeat(batch_size, query_length, 1)
+        # kv_idxs = torch.arange(self.num_heads, dtype=torch.int32, device=query.device)[None, None, :].repeat(batch_size, query_length, 1)
+        # print(kv_idxs.size(), selected_experts.size())
         hidden_states = mokv_attention(
             q=query, # .contiguous(),
             k=key, # .contiguous(),
             v=value, # .contiguous(),
-            kv_idxs=kv_idxs,
+            kv_idxs=selected_experts,
             causal=self.causal,
             sm_scale=self.attention_multiplier
         )
@@ -193,5 +297,18 @@ class MoKVAttention(nn.Module):
 
         hidden_states = self.c_proj(hidden_states)
         hidden_states = self.dropout(hidden_states)
+        aux_loss = (
+            self._compute_switch_loss(
+                logits=router_logits,
+                probs=torch.softmax(router_logits, dim=-1),
+                expert_frequency=expert_frequency
+            )
+            if self.training
+            else 0
+        )
+
+ 
+        add_aux_loss(aux_loss)
+
 
         return hidden_states
